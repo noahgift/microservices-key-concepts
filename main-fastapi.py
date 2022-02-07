d8341e69aea8f220d75db1d96355497685b6b11d
Noah Gift <noah.gift@gmail.com>
2022-02-07 18:16:01 +0000

    adding fastapi

---
+++ main-fastapi.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+import uvicorn
+from logic import add
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello"}
+
+@app.get("/add/{num1}/{num2}")
+async def add(num1: int, num2: int):
+    """Add two numbers together"""
+
+    total = add(num1,num2)
+    return {"total": total}
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=8080, host='0.0.0.0')
